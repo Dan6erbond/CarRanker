eeb62607a07448ac91e0dd5c6b8d5c7bd9d22688
Dan6erbond <moravrav@gmail.com>
2021-01-15 14:29:17 +0100

    Serialize car returned by scraper with marshmallow

--- app/routes/api/v1/cars.py
+++ app/routes/api/v1/cars.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 from flask import current_app as app
 from flask import jsonify, request
 
+from ....database.models import Car, CarSchema
+
 
 def get_car_data(url):
     if url.startswith("https://www.carforyou.ch"):
@@ -23,4 +25,10 @@ def scrape_car():
         url = request.form.get("url") or request.data.get("url")
     else:
         url = "https://www.carforyou.ch/en/auto/saloon/vw/passat/20-bluetdi-comfortline-dsg-214965"
-    return jsonify(get_car_data(url))
+
+    car = get_car_data(url)
+    car = Car(data=car, **car)
+
+    car_schema = CarSchema()
+
+    return car_schema.dump(car)
