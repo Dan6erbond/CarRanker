d5588cd07268ac4426f2a4e747f949ea31cfd704
Dan6erbond <moravrav@gmail.com>
2021-01-16 22:11:20 +0100

    Fix JSON type serialization and deserialization for APIs

--- app/exts/ma.py
+++ app/exts/ma.py
@@ -12,10 +12,10 @@ class Json(fields.Field):
     def _serialize(self, value, attr, obj, **kwargs):
         if value is None:
             return None
-        return json.dumps(value)
+        return json.loads(value)
 
     def _deserialize(self, value, attr, data, **kwargs):
         try:
-            return [json.loads(value)]
+            return json.dumps(value)
         except ValueError as error:
             raise ValidationError("Pin codes must contain only digits.") from error
