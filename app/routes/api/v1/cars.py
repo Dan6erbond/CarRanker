95aba3daf2fb9f743fc2ddbe5223014c81affc0b
Dan6erbond <moravrav@gmail.com>
2021-01-16 22:13:05 +0100

    Load car data in helper method to account for various site models, create car input model for POST request and save to DB

--- app/routes/api/v1/cars.py
+++ app/routes/api/v1/cars.py
@@ -1,12 +1,14 @@
 import json
+from datetime import date, datetime
 
 import requests
+from app.database.models.car import BodyType, CarSchema, DriveType
 from bs4 import BeautifulSoup
-from flask import request
-from flask_restplus import Resource, reqparse
+from flask_restplus import Resource, fields
 
 from ....database.db import db
-from ....database.models import Car, CarSchema
+from ....database.models import Car
+from ....database.models.make import Make
 from ....helpers import snake_case_keys
 from .api import api
 
@@ -18,8 +20,24 @@ def get_car_data(url):
         soup = BeautifulSoup(page.content, 'html.parser')
 
         data = soup.find(id="__NEXT_DATA__")
-        data = json.loads(data.contents[0])
-        return data["props"]["pageProps"]["listing"]
+        contents = data.contents[0]
+        raw_data = json.loads(contents)
+        raw_data = raw_data["props"]["pageProps"]["listing"]
+
+        data = json.loads(contents)
+        data = data["props"]["pageProps"]["listing"]
+        data = snake_case_keys(data)
+
+        first_registration_date = datetime(
+            data["first_registration_date"]["year"],
+            data["first_registration_date"]["month"],
+            1)
+
+        data["first_registration_date"] = first_registration_date
+        data["body_type"] = BodyType.get_body_type(data["body_type"]).value
+        data["drive_type"] = DriveType.get_drive_type(data["drive_type"]).value
+
+        return data, raw_data
 
 
 car_fields = api.model("Car", {
@@ -43,31 +61,47 @@ car_fields = api.model("Car", {
     "consumption_combined": fields.Integer,
 })
 
+car_input = api.model("CarInput", {
+    "url": fields.String(description="A URL to the car listing.", required=True)
+})
+
 
 @api.route("/cars")
 class CarsController(Resource):
     def get(self):
         cars_schema = CarSchema(many=True)
-
         cars = Car.query.all()
-        items = cars_schema.dump(cars)
 
-        return {"cars": items}
+        return cars_schema.dump(cars)
 
+    @api.doc(body=car_input)
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('rate', type=int, help='Rate to charge for this resource')
-        args = parser.parse_args()
+        url = api.payload["url"]
 
-        url = request.form.get("url") or request.data.get("url")
+        data, raw_data = get_car_data(url)
 
-        car = get_car_data(url)
-        car = snake_case_keys(car)
-        car = Car(data=car, url=url, variant=car["type"], **car)
+        make = Make.query.filter(Make.name == data["make"]).first()
+        if not make:
+            make = Make(name=data["make"])
+            db.session.add(make)
+        make = Make.query.filter(Make.name == data["make"]).first()
+
+        del data["make"]
+        data["make_id"] = make.id
+
+        del data["images"]
+
+        def default(o):
+            if isinstance(o, (date, datetime)):
+                return o.isoformat()
+
+        car_schema = CarSchema()
+        car = Car(
+            data=json.dumps(raw_data, default=default),
+            url=url, variant=data["type"],
+            **data)
 
         db.session.add(car)
         db.session.commit()
-
-        car_schema = CarSchema()
 
         return car_schema.dump(car)
