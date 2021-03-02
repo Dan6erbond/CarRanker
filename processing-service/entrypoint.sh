fbf4a7150cd75b5b4e741472ffcda24fd1df90b0
Dan6erbond <moravrav@gmail.com>
2021-03-02 10:04:37 +0100

    Add wait for PostgreSQL in entrypoint

--- processing-service/entrypoint.sh
+++ processing-service/entrypoint.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+set -e
+
+timer="5"
+
+until runuser -l postgres -c 'pg_isready' 2>/dev/null; do
+  >&2 echo "Postgres is unavailable - sleeping for $timer seconds"
+  sleep $timer
+done
+
+>&2 echo "Postgres is up - running migrations"
+
 pip install -e .
 
 alembic upgrade head
+
+>&2 echo "Migrations complete - running Flask"
 
 python runner.py
