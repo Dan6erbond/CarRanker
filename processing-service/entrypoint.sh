df8e2e5c0af7493e9d4af4e2a08c2d7efdcee5a7
Dan6erbond <moravrav@gmail.com>
2021-03-02 10:07:30 +0100

    Remove wait for in entrypoint script

--- processing-service/entrypoint.sh
+++ processing-service/entrypoint.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
 
-set -e
-
-timer="5"
-
-until runuser -l postgres -c 'pg_isready' 2>/dev/null; do
-  >&2 echo "Postgres is unavailable - sleeping for $timer seconds"
-  sleep $timer
-done
-
->&2 echo "Postgres is up - running migrations"
-
 pip install -e .
 
 alembic upgrade head
-
->&2 echo "Migrations complete - running Flask"
 
 python runner.py
