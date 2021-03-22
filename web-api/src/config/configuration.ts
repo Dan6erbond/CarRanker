96a3873a4efc9136f0dfd7ca15e5f138c1f77998
Dan6erbond <moravrav@gmail.com>
2021-03-22 13:27:09 +0100

    Add JWT key to Docker secrets and NestJS config

--- web-api/src/config/configuration.ts
+++ web-api/src/config/configuration.ts
@@ -1,6 +1,17 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-  auth: {
-    jwtKey: process.env.JWT_KEY,
-  },
-});
+import { readFileSync } from "fs";
+
+export default () => {
+  let jwtKey = process.env.JWT_KEY;
+
+  if (process.env.JWT_KEY_FILE) {
+    const contents = readFileSync(process.env.JWT_KEY_FILE, "utf8");
+    jwtKey = contents.split(/\r?\n/)[0];
+  }
+
+  return {
+    port: parseInt(process.env.PORT, 10) || 3000,
+    auth: {
+      jwtKey,
+    },
+  };
+};
